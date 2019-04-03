16fbfa3473f8fcebfc451de3f0c516acd23e258a
Daniel Schneider <daschne8@gmail.com>
2019-04-03 23:39:46 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -3,7 +3,7 @@ module Players
   class Computer < Player
     def move(board)
       valid_positions = board.get_valid_cell_positions
-      valid_positions[rand(0..valid_positions.length)]
+      valid_positions[rand(0...valid_positions.length)]
 
     end
   end
